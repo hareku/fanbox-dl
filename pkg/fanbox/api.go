005d0f3636b1bc95e8c7b964d011743389e76172
hareku <hareku908@gmail.com>
2020-12-03 23:33:13 +0000

    chore: tweak file structure

--- pkg/fanbox/api.go
+++ pkg/fanbox/api.go
@@ -1,5 +1,11 @@
 package fanbox
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
 // ListCreator represents the response of https://api.fanbox.cc/post.listCreator.
 type ListCreator struct {
 	Body ListCreatorBody `json:"body"`
@@ -56,4 +62,24 @@ func (b *PostBody) OrderedImageMap() []Image {
 	}
 
 	return images
+}
+
+// Request sends a request to FANBOX with credentials.
+func Request(ctx context.Context, sessid string, url string) (*http.Response, error) {
+	client := http.Client{}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("http request building error: %w", err)
+	}
+
+	req.Header.Set("Cookie", fmt.Sprintf("FANBOXSESSID=%s", sessid))
+	req.Header.Set("Origin", "https://www.fanbox.cc") // If Origin header is not set, FANBOX returns HTTP 400 error.
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("http response error: %w", err)
+	}
+
+	return resp, nil
 }
